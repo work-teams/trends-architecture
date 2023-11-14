c1a05377245d07a61cbf96f832c38796fb030ae9
Jorge Marin <jm.dev97@gmail.com>
2023-11-13 21:59:44 -0500

    upgrade admin sin delete

--- src/vue-front/src/components/AdminForm.vue
+++ src/vue-front/src/components/AdminForm.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
-        <h2>Gestión de Administradores</h2>
-        <div class="form">
-            <label>Nombres:</label>
-            <input type="text" v-model="nombres" placeholder="Nombres">
-            <label>Apellidos:</label>
-            <input type="text" v-model="apellidos" placeholder="Apellidos">
-            <label>Correo:</label>
-            <input type="email" v-model="correo" placeholder="Correo">
-            <label>Teléfono:</label>
-            <input type="tel" v-model="telefono" placeholder="Teléfono">
+    <div class="container">
+        <div class="form-container">
+            <h2>Gestión de Administradores</h2>
+            <div class="form">
+                <label>Nombres:</label>
+                <input type="text" v-model="nombres" placeholder="Nombres">
+                <label>Apellidos:</label>
+                <input type="text" v-model="apellidos" placeholder="Apellidos">
+                <label>Correo:</label>
+                <input type="email" v-model="correo" placeholder="Correo">
+                <label>Teléfono:</label>
+                <input type="tel" v-model="telefono" placeholder="Teléfono">
 
-            <button @click="agregarAdmin" v-if="!isEdit">Agregar Administrador</button>
-            <button @click="actualizarAdmin" v-if="isEdit">Actualizar Administrador</button>
-            <button @click="limpiarCampos">Limpiar Campos</button>
-            <button @click="volver">Volver</button>
+                <button @click="agregarAdmin" v-if="!isEdit">Agregar Administrador</button>
+                <button @click="actualizarAdmin" v-if="isEdit">Actualizar Administrador</button>
+                <button @click="limpiarCampos">Limpiar Campos</button>
+                <button @click="volver">Volver</button>
+            </div>
         </div>
 
-        <h2>Lista de Administradores</h2>
         <div class="admin-list">
+            <h2>Lista de Administradores</h2>
             <ul>
                 <li v-for="(admin, index) in administradores" :key="index">
                     {{ admin.nombres }} {{ admin.apellidos }} - {{ admin.correo }} - {{ admin.telefono }}
@@ -28,6 +30,7 @@
         </div>
     </div>
 </template>
+
   
 <script>
 export default {
@@ -193,6 +196,9 @@ export default {
             this.actualizarListaAdmins();
         },
     },
+    mounted() {
+        this.actualizarListaAdmins(); // Llama a la función para cargar la lista cuando la vista esté montada
+    }
 };
 </script>
   
@@ -200,15 +206,20 @@ export default {
 .container {
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    /* Alinear elementos en la parte superior */
     height: 100vh;
+    margin-top: 20px;
+    /* Ajustar el margen superior según tus necesidades */
+}
+
+.form-container {
+    margin-right: 20px;
+    /* Espaciado entre el formulario y la lista */
 }
 
 .form {
     text-align: center;
-}
-
-.form {
     display: grid;
     grid-template-columns: 1fr;
     gap: 10px;
@@ -244,10 +255,11 @@ button {
 .admin-list {
     border: 1px solid #ccc;
     padding: 10px;
-    margin-top: 20px;
-    max-width: 600px;
-    margin: 0 auto;
-    overflow: auto;
+    overflow-y: auto;
+    /* Agregado para la barra de desplazamiento vertical */
+    max-height: 600px;
+    /* Altura máxima para la lista antes de agregar la barra de desplazamiento */
+    max-width: 1000px;
 }
 
 ul {
