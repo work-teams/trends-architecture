dba93ac833afb34d20ab765012fe4732cd9dac8d
Wiston Pastor <wiston.pastor@unmsm.edu.pe>
2023-11-29 23:05:41 -0500

    upload implementation notifi

--- src/extranet/react-front/src/index.js
+++ src/extranet/react-front/src/index.js
@@ -6,6 +6,8 @@ import WelcomeView from './views/WelcomeView';
 import DataValidationForm from './views/DataValidationForm';
 import reportWebVitals from './components/reportWebVitals';
 import DataValidationFace from './views/DataValidationFace';
+import Notification from './views/Notification';
+import NotificationF from './views/NotificationF';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +17,8 @@ root.render(
         <Route path="/" element={<WelcomeView />} />
         <Route path="/data-validation-form" element={<DataValidationForm />} />
         <Route path="/data-validation-face" element={<DataValidationFace />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/notificationf" element={<NotificationF />} />
       </Routes>
     </Router>
   </React.StrictMode>,
